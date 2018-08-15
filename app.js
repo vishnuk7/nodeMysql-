1a7870191e2f1bcf88eb26c67829367b53ef56e0
Vishnu <vishnu.kraman89.outlook.com>
2018-08-16 00:14:40 +0530

    create connection with mysql

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mysql = require('mysql');
 
 const app = express();
 
+//create connection with sql
+const db = mysql.createConnection();
+
 app.listen('3000',()=>{
   console.log("Server started on 3000");
 });
