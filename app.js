1aee031f81d601548c857b95ed5e45b80d01eba5
Vishnu <vishnu.kraman89.outlook.com>
2018-08-16 00:26:24 +0530

    create DB

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ const mysql = require('mysql');
 const app = express();
 
 //create connection with sql
-const db = mysql.createConnection();
+const db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  // database:'nodemysql'
+});
+
+//conect
+db.connect((err)=>{
+  console.log("MySql Connected ...");
+});
+
+//Create DB
+app.get('/createdb',(req,res)=>{
+  let sql = 'CREATE DATABASE nodemysql';
+  db.query(sql,(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send('Database Created');
+  });
+})
 
 app.listen('3000',()=>{
   console.log("Server started on 3000");
